2fc757754c2b10aeec7453b985bff571c853042e
Ilham-Syed <ilhamsyed1310@gmail.com>
2023-08-20 00:46:59 +0530

    Basic styles added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app=express();
 const path=require('path');
+const ejsMate=require('ejs-mate');
 const Campground=require('./models/campground');
 const mongoose=require('mongoose');
 const methodOverride=require('method-override');
@@ -13,12 +14,16 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
+app.engine('ejs',ejsMate);
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'))
 
+
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
+
+
 
 app.get('/',(req,res)=>{
     res.render('home');
--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,6 @@
 
 const mongoose=require('mongoose');
+const axios = require('axios');
 const Campground = require('../models/campground');
 const {descriptors,places}=require('./seedhelpers');
 const cities=require('./cities');
@@ -13,17 +14,37 @@ db.once("open", () => {
 const sample=(array)=>{
     return array[Math.floor(Math.random()*array.length)];
 }
+
+async function seedImg() {
+    try {
+      const resp = await axios.get('https://api.unsplash.com/photos/random', {
+        params: {
+          client_id: '**Your API client ID here**',
+          collections: 483251,
+        },
+      })
+      return resp.data.urls.small
+    } catch (err) {
+      console.error(err)
+    }
+  } 
 const seedDB=async ()=>{
     await Campground.deleteMany({});
-    for(let i=0;i<50;i++){
+    for(let i=0;i<30;i++){
         const random1000=Math.floor(Math.random()*1000);
+        const price=Math.floor(Math.random()*300)+100;
         const camp=new Campground({
             location:`${cities[random1000].city},${cities[random1000].state}`,
-            title:`${sample(descriptors)} ${sample(places)}`
+            title:`${sample(descriptors)} ${sample(places)}`,
+            image:await seedImg(),
+            description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, iste, quos consequuntur quibusdam optio impedit sapiente id magni ipsa explicabo, voluptas illum veritatis rem expedita magnam velit perspiciatis dolorum vero.',
+            price
         })
         await camp.save();
     }
 }
+
+
 
 seedDB().then(()=>{
     console.log("Connection closed");
